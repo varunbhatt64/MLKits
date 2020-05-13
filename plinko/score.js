22c905cc5f498cd8680c69b857ac40222282ac85
varunbhatt64 <60150618+varunbhatt64@users.noreply.github.com>
2020-05-13 10:02:55 +0530

    implemented initial KNN

--- plinko/score.js
+++ plinko/score.js
@@ -1,8 +1,32 @@
+const outputs = [];
+const k = 3;
+const predictionPoint = 300;
+
+
 function onScoreUpdate(dropPosition, bounciness, size, bucketLabel) {
   // Ran every time a balls drops into a bucket
+  outputs.push([dropPosition, bounciness, size, bucketLabel]);
+  //console.log(Outputs);
 }
 
 function runAnalysis() {
   // Write code here to analyze stuff
+  const bucket = _.chain(outputs)
+    .map(r => [distance(r[0]), r[3]])
+    .sortBy(r => r[0])
+    .slice(0, k)
+    .countBy(r => r[1])
+    .toPairs()
+    .sortBy(r => r[0])
+    .last()
+    .first()
+    .parseInt()
+    .value()
+
+    console.log('Your ball will probably fall into bucket #'+ bucket);
+}
+
+function distance(point) {
+  return Math.abs(point - predictionPoint);
 }
 
